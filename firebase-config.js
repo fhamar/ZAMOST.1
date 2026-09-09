23db608e12b29371aff9ede07d0a0f58c86a6fb1
fhamar <fhamarzamost@gmail.com>
2026-09-10 02:48:44 +0530

    Update Firebase SDK to version 12.1.0

--- firebase-config.js
+++ firebase-config.js
@@ -2,11 +2,9 @@
 // FHAMAR.ZAMOST Firebase Config
 // =====================================
 
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-app.js";
-
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-auth.js";
-
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-firestore.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDpyBWCS7hquXoKcEfGSYDFi7epMNHTTTg",
@@ -22,3 +20,5 @@ const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+export { app };
+
